c4dc68b5ad352e3e4319b3ea9734221ce764a168
Syeda Rabia <91334836+SRAABIA@users.noreply.github.com>
2024-08-08 13:47:43 +0500

    Count # Windows
    Revit

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Globalization;
+
+namespace WindowCounter
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class CollectWindows : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Windows);
+            IList<Element> windows = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+            TaskDialog.Show("Windows Count", string.Format("{0} windows found", windows.Count));
+            return Result.Succeeded;
+        }
+    }
+}
